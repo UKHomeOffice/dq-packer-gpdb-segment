cfada0d5916cd5c1045a227e8fbeeceed86d2c41
david-wirelab <info@wirelab.co.uk>
2018-07-12 12:37:50 +0100

    break out swap to bootstrap and update all yum libraries

--- bootstrap-raid.sh
+++ bootstrap-raid.sh
@@ -20,4 +20,10 @@ else
   echo "/dev/mapper/secure /gpdb xfs nodev,noatime,inode64,allocsize=16m 0 0" >> /etc/fstab
   mdadm -E -s -v >> /etc/mdadm.conf
   tar -C / -xzvf /home/centos/md0.tgz
+  dd if=/dev/zero of=/dm-2 count=32768 bs=1MiB
+  chmod 600 /dm-2
+  mkswap /dm-2
+  swapon -v /dm-2
+  cp /etc/fstab /etc/fstab.bak
+  echo "/dm-2 swap swap defaults 0 0" >> /etc/fstab
 fi
