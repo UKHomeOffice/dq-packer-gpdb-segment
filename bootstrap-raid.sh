b81310edec5b45016a078e7c5631380289b67703
david-wirelab <info@wirelab.co.uk>
2018-07-13 11:48:28 +0100

    remove exit, remove static mappings, add DHCP map

--- bootstrap-raid.sh
+++ bootstrap-raid.sh
@@ -3,11 +3,11 @@ export INSTANCE_ID=$(curl http://169.254.169.254/latest/user-data)
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 set -e
-set -x
 
 if [ -b /dev/md0 ]; then
   aws --region eu-west-2 ssm get-parameter --name ${INSTANCE_ID} --with-decryption --query 'Parameter.Value' --output text | cryptsetup luksOpen /dev/md0 secure
   mount -o nodev,noatime,inode64,allocsize=16m /dev/mapper/secure /gpdb
+  dhclient
 else
   mdadm --create /dev/md0 --level=0 --raid-devices=$(echo /dev/xvd[b-z] | wc -w) $(echo /dev/xvd[b-z])
   aws --region eu-west-2 ssm put-parameter --name ${INSTANCE_ID} --value "$(uuidgen)" --overwrite --type "SecureString"
@@ -17,13 +17,10 @@ else
   mkdir /gpdb
   mkfs.xfs /dev/mapper/secure
   mount -o nodev,noatime,inode64,allocsize=16m /dev/mapper/secure /gpdb
-  echo "/dev/mapper/secure /gpdb xfs nodev,noatime,inode64,allocsize=16m 0 0" >> /etc/fstab
-  mdadm -E -s -v >> /etc/mdadm.conf
-  tar -C / -xzvf /home/centos/md0.tgz
-  sudo dd if=/dev/zero of=/dm-2 count=65536 bs=1MiB
-  sudo chmod 600 /dm-2
-  sudo mkswap /dm-2
-  sudo swapon -v /dm-2
-  sudo cp /etc/fstab /etc/fstab.bak
+  dd if=/dev/zero of=/dm-2 count=65536 bs=1MiB
+  chmod 600 /dm-2
+  mkswap /dm-2
+  swapon -v /dm-2
+  cp /etc/fstab /etc/fstab.bak
   echo "/dm-2 swap swap defaults 0 0" >> /etc/fstab
 fi
