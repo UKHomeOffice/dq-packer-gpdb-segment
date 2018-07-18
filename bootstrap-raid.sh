34684e3b38d3d24978d45f2a14342c02b98867e0
david-wirelab <info@wirelab.co.uk>
2018-07-18 16:08:09 +0100

    update bootstrap

--- bootstrap-raid.sh
+++ bootstrap-raid.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-export INSTANCE_ID=$(curl http://169.254.169.254/latest/user-data)
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 set -e
+set -x
 
-if [ -b /dev/md0 ]; then
-  aws --region eu-west-2 ssm get-parameter --name ${INSTANCE_ID} --with-decryption --query 'Parameter.Value' --output text | cryptsetup luksOpen /dev/md0 secure
-  mount -o nodev,noatime,inode64,allocsize=16m /dev/mapper/secure /gpdb
+if ! $(df -T | grep /dev/nvme0n1); then
+  mkfs.xfs -f /dev/nvme0n1
+  mount -o nodev,noatime,inode64,allocsize=16m /dev/nvme0n1 /ephemeral
+  dd if=/dev/zero of=/ephemeral/swap count=32768 bs=1MiB
+  chmod 600 /ephemeral/swap
+  mkswap /ephemeral/swap
+  swapon -v /ephemeral/swap
   dhclient
 else
-  mdadm --create /dev/md0 --level=0 --raid-devices=$(echo /dev/xvd[b-z] | wc -w) $(echo /dev/xvd[b-z])
-  aws --region eu-west-2 ssm put-parameter --name ${INSTANCE_ID} --value "$(uuidgen)" --overwrite --type "SecureString"
-  aws --region eu-west-2 ssm get-parameter --name ${INSTANCE_ID} --with-decryption --query 'Parameter.Value' --output text | cryptsetup -y --cipher=aes-cbc-essiv:sha256 luksFormat /dev/md0
-  aws --region eu-west-2 ssm get-parameter --name ${INSTANCE_ID} --with-decryption --query 'Parameter.Value' --output text | cryptsetup luksOpen /dev/md0 secure
-  cd /
-  mkdir /gpdb
-  mkfs.xfs /dev/mapper/secure
-  mount -o nodev,noatime,inode64,allocsize=16m /dev/mapper/secure /gpdb
-  dd if=/dev/zero of=/dm-2 count=65536 bs=1MiB
-  chmod 600 /dm-2
-  mkswap /dm-2
-  swapon -v /dm-2
-  cp /etc/fstab /etc/fstab.bak
-  echo "/dm-2 swap swap defaults 0 0" >> /etc/fstab
+  mount -o nodev,noatime,inode64,allocsize=16m /dev/nvme0n1 /ephemeral
+  dd if=/dev/zero of=/ephemeral/swap count=32768 bs=1MiB
+  chmod 600 /ephemeral/swap
+  mkswap /ephemeral/swap
+  swapon -v /ephemeral/swap
+  dhclient
 fi
