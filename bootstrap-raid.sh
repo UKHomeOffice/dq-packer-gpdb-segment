bf28eae055885459599f225ea564b51f5a37ccf9
david-wirelab <info@wirelab.co.uk>
2018-07-18 16:10:05 +0100

    up swap size

--- bootstrap-raid.sh
+++ bootstrap-raid.sh
@@ -7,14 +7,14 @@ set -x
 if ! $(df -T | grep /dev/nvme0n1); then
   mkfs.xfs -f /dev/nvme0n1
   mount -o nodev,noatime,inode64,allocsize=16m /dev/nvme0n1 /ephemeral
-  dd if=/dev/zero of=/ephemeral/swap count=32768 bs=1MiB
+  dd if=/dev/zero of=/ephemeral/swap count=65536 bs=1MiB
   chmod 600 /ephemeral/swap
   mkswap /ephemeral/swap
   swapon -v /ephemeral/swap
   dhclient
 else
   mount -o nodev,noatime,inode64,allocsize=16m /dev/nvme0n1 /ephemeral
-  dd if=/dev/zero of=/ephemeral/swap count=32768 bs=1MiB
+  dd if=/dev/zero of=/ephemeral/swap count=65536 bs=1MiB
   chmod 600 /ephemeral/swap
   mkswap /ephemeral/swap
   swapon -v /ephemeral/swap
