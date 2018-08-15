a83f7c65997a2607a970ba77fd3d1efa5b2cb5e4
david-wirelab <info@wirelab.co.uk>
2018-08-15 13:59:15 +0100

    update ephemeral folder setup script

--- ephemeral_folders.sh
+++ ephemeral_folders.sh
@@ -1,7 +1,6 @@
 #------------------------------------------
 # /gpdb/ directories
 #------------------------------------------
-su gpadmin
 mkdir -p /gpdb/master/backups
 mkdir -p /gpdb/master/backups_NRT
 mkdir -p /gpdb/master/backups_NRT/db_dumps
@@ -10,7 +9,7 @@ mkdir -p /gpdb/master/backups_rpt_ext
 mkdir -p /gpdb/master/gpcrondump_backups
 mkdir -p /gpdb/master/gpcrondump_backups_NRT
 mkdir -p /gpdb/master/gpcrondump_backups_rpt_ext
-exit
+chown -R gpadmin:gpadmin /gpdb
 find /gpdb/master -type d -exec chmod 775 {} \;
 find /gpdb/master/gpcrondump_backups_NRT -type f -exec chmod -R 700 {} \;
 find /gpdb/master/gpcrondump_backups_rpt_ext -type f -exec chmod -R 700 {} \;
